e643d09be579adf4de1e9e2d6fadce05d6f38c0f
David Rigamonte <david.rigamonte@tecnospeed.com.br>
2021-05-08 13:53:23 -0300

    feat: simple login route

--- index.js
+++ index.js
@@ -4,9 +4,13 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
 
+const helmet = require('helmet')
+
 const router = require('./routes')
 
-app.use(router)
+app.use(helmet())
+app.use(express.json())
+app.use('/v1', router)
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,34 @@
 const express = require('express')
 const router = express.Router()
 
-router.get('/', (req, res) => {
-  res.send('Hello World!')
+const jwt = require('jsonwebtoken')
+const accessTokenSecret = process.env.JWT_ACCESS_TOKEN_SECRET
+
+router.get('/status', (req, res) => {
+  res.send({ status: 'OK' })
+})
+
+router.post('/login', (req, res) => {
+  const { username, password } = req.body
+
+  const users = require('../resources/mockUsers.json')
+  const user = users.find((u) => {
+    return u.username === username && u.password === password
+  })
+
+  if (user) {
+    const accessToken = jwt.sign(
+      { username: user.username, role: user.role },
+      accessTokenSecret,
+      { expiresIn: '1d' }
+    )
+
+    return res.json({
+      accessToken,
+    })
+  }
+
+  return res.status(401).json({ message: 'Username and password incorrect!' })
 })
 
 module.exports = router
